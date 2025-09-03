adac10aea33a70cbd002fa38b8695b345080a6c2
hexpreso <32096750+hexpreso@users.noreply.github.com>
2025-09-03 10:01:32 +0200

    fix(zsh): make installer repo URL configurable via DOTFILES_REPO env var

--- scripts/install-zsh-keybinds.sh
+++ scripts/install-zsh-keybinds.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
+REPO_URL="${DOTFILES_REPO:-https://raw.githubusercontent.com/rafareborn/dotfiles/master}"
+
 mkdir -p "$HOME/.config/zsh"
-curl -fsSL "https://raw.githubusercontent.com/rafareborn/dotfiles/master/.config/zsh/zsh-emacs-keybinds.zsh" -o "$HOME/.config/zsh/zsh-emacs-keybinds.zsh"
+curl -fsSL "$REPO_URL/.config/zsh/zsh-emacs-keybinds.zsh" -o "$HOME/.config/zsh/zsh-emacs-keybinds.zsh"
 
 if [ -f "$HOME/.zshrc" ] && ! grep -q "zsh-emacs-keybinds.zsh" "$HOME/.zshrc"; then
     echo "[ -f ~/.config/zsh/zsh-emacs-keybinds.zsh ] && source ~/.config/zsh/zsh-emacs-keybinds.zsh" >> "$HOME/.zshrc"
