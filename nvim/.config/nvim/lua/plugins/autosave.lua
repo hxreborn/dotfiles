abf399c5cd2274af85025eaa6c3285c8d0eff60c
hxreborn <hxreborn@duck.com>
2026-04-04 17:15:39 +0200

    feat: add autosave plugin with 300ms debounce
    Signed-off-by: hxreborn <hxreborn@duck.com>

---
+++ nvim/.config/nvim/lua/plugins/autosave.lua
@@ -0,0 +1,25 @@
+return {
+  "pocco81/auto-save.nvim",
+  event = { "InsertLeave", "TextChanged" },
+  opts = {
+    enabled = true,
+    execution_message = {
+      message = function() return "" end,
+    },
+    trigger_events = {
+      immediate_save = { "BufLeave", "FocusLost" },
+      defer_save = { "InsertLeave", "TextChanged" },
+    },
+    condition = function(buf)
+      local fn = vim.fn
+      local utils = require("auto-save.utils.data")
+
+      if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+        return true
+      end
+      return false
+    end,
+    write_all_buffers = false,
+    debounce_delay = 300,
+  },
+}
