6ef0fa294f91989ab85224002f7d208687e3448d
hxreborn <hxreborn@duck.com>
2026-04-05 22:47:26 +0200

    fix(nvim): remove redundant config for formatting
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,10 +9,14 @@
 
 -- Relative numbers in normal mode, absolute in insert
 vim.api.nvim_create_autocmd("InsertEnter", {
-  callback = function() vim.opt.relativenumber = false end,
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
-  callback = function() vim.opt.relativenumber = true end,
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
 })
 
 -- Disable diagnostics for markdown files
@@ -20,43 +24,5 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function(args)
     vim.diagnostic.enable(false, { bufnr = args.buf })
-  end,
-})
-
--- Auto-format Kotlin files with ktlint on save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = vim.api.nvim_create_augroup("ktlint_format", { clear = true }),
-  pattern = "*.kt",
-  callback = function()
-    -- Only format if we're in a Gradle project
-    local gradle_file = vim.fn.findfile("build.gradle.kts", ".;")
-    if gradle_file == "" then
-      gradle_file = vim.fn.findfile("build.gradle", ".;")
-    end
-
-    if gradle_file ~= "" then
-      vim.lsp.buf.format({
-        name = "conform.nvim",
-        async = false,
-      })
-    end
-  end,
-})
-
--- Run ktlint check when Kotlin files are saved
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("ktlint_check", { clear = true }),
-  pattern = "*.kt",
-  callback = function()
-    -- Only check if we're in a Gradle project
-    local gradle_file = vim.fn.findfile("build.gradle.kts", ".;")
-    if gradle_file == "" then
-      gradle_file = vim.fn.findfile("build.gradle", ".;")
-    end
-
-    if gradle_file ~= "" then
-      local lint = require("lint")
-      lint.try_lint()
-    end
   end,
 })
