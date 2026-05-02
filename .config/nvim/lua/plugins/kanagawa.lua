73e25f70c657271269ac8f523ead9385f12cf82c
hxreborn <hxreborn@duck.com>
2026-05-02 20:12:46 +0200

    update fastfetch accent colors, disable kanagawa, fix shell-switch, untrack kitty theme files
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,24 +1,6 @@
 return {
 	{
 		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		opts = {
-			compile = false,
-			undercurl = true,
-			commentStyle = { italic = true },
-			functionStyle = { bold = true },
-			keywordStyle = { bold = true },
-			statementStyle = { bold = true },
-			typeStyle = { bold = true, italic = true },
-			transparent = false,
-			terminalColors = true,
-			theme = "wave", -- "wave" | "dragon" | "lotus"
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "kanagawa",
-		},
+		enabled = false,
 	},
 }
